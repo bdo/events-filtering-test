2e9f9420d62d84498f1c0be0cf69b1784d2106ea
Benoit d'Oncieu <bdoncieu@gmail.com>
2016-12-14 15:48:31 +0100

    Add chai-webdriver-promised again to simplify code (it previously failed but due to an old version of npm in circle)

--- test/features/step_definitions/observation-steps.js
+++ test/features/step_definitions/observation-steps.js
@@ -1,8 +1,7 @@
 export default function () {
 
   this.Then(/^I should see '(.*)'$/, async function (text) {
-    const bodyText = await this.driver.findElement({css: 'body'}).getText();
-    expect(bodyText).to.contain(text);
+    expect('body').dom.to.have.text(text)
   });
 
 };
